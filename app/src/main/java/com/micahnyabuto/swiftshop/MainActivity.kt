6f286961a7e21db778710f84afe84de5c5079739
Dev-Micah <atembamicah@gmail.com>
2024-11-14 13:09:57 +0300

    update

--- app/src/main/java/com/micahnyabuto/swiftshop/MainActivity.kt
+++ app/src/main/java/com/micahnyabuto/swiftshop/MainActivity.kt
@@ -17,7 +17,8 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.micahnyabuto.swiftshop.ui.theme.SwiftShopTheme
+import com.example.compose.SwiftShopTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

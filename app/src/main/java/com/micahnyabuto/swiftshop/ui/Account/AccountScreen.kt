36a9c61325e7d116ced2c6271c17afb45276f2d2
Micah Nyabuto <atembamicah@gmail.com>
2025-04-09 23:23:30 +0300

    Update

---
+++ app/src/main/java/com/micahnyabuto/swiftshop/ui/Account/AccountScreen.kt
@@ -0,0 +1,9 @@
+package com.micahnyabuto.swiftshop.ui.Account
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AccountScreen(){
+    Text("Account Screen")
+}
--- app/src/main/java/com/micahnyabuto/swiftshop/ui/checkout/CartItem.kt
+++
@@ -1,8 +0,0 @@
-package com.micahnyabuto.swiftshop.ui.checkout
-
-import com.micahnyabuto.swiftshop.data.Product
-
-data class CartItem(
-    val product: Product,
-    var quantity: Int=1
-)

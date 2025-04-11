ec8cbbd91497adfa5484ea5fe826f89d3afd701b
Micah Nyabuto <atembamicah@gmail.com>
2025-04-11 21:51:02 +0300

    Added Homescreen TopBar

--- app/src/main/java/com/micahnyabuto/swiftshop/ui/home/HomeScreen.kt
+++ app/src/main/java/com/micahnyabuto/swiftshop/ui/home/HomeScreen.kt
@@ -1,9 +1,71 @@
 package com.micahnyabuto.swiftshop.ui.home
 
+import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose.primaryLight
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(){
-    Text("Swift Shop")
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                navigationIcon= {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                title = {
+                    Text("Swift Shop",
+
+                        )
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Search product"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = primaryLight,
+                    titleContentColor = White,
+                    navigationIconContentColor = White,
+                    actionIconContentColor = White
+
+                ),
+                scrollBehavior = scrollBehavior
+
+            )
+        }
+    ) {
+
+    }
+
+}
+@Preview
+@Composable
+fun HomesScreenPreview(){
+    HomeScreen()
 }

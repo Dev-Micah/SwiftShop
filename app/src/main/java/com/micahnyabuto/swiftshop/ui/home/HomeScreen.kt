c0e44be484c89dff81d2d53d5f5ccc106872e922
Micah Nyabuto <atembamicah@gmail.com>
2025-04-11 22:20:12 +0300

    Update

--- app/src/main/java/com/micahnyabuto/swiftshop/ui/home/HomeScreen.kt
+++ app/src/main/java/com/micahnyabuto/swiftshop/ui/home/HomeScreen.kt
@@ -1,6 +1,11 @@
 package com.micahnyabuto.swiftshop.ui.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.R
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Menu
@@ -15,8 +20,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.primaryLight
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -61,6 +72,19 @@ fun HomeScreen(){
         }
     ) {
 
+    }
+
+}
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+
+    Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        Image(
+            modifier = modifier.size(200.dp),
+            painter = painterResource(R.drawable.loading_img),
+            contentDescription = "Loading",
+            contentScale = ContentScale.Crop
+        )
     }
 
 }

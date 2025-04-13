15b0244df288bae5ece851a9fec94e875bba9d7d
Micah Nyabuto <atembamicah@gmail.com>
2025-04-13 23:12:56 +0300

    Update

---
+++ app/src/main/java/com/micahnyabuto/swiftshop/ui/order/OrderPlacedScreen.kt
@@ -0,0 +1,9 @@
+package com.micahnyabuto.swiftshop.ui.order
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OrderPlacedScreen(){
+    Text("Order Placed Screen")
+}

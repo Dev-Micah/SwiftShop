b367a007914057e370a355ea10ef4dff497dc148
Micah Nyabuto <atembamicah@gmail.com>
2025-04-13 23:13:48 +0300

    Update

--- app/src/main/java/com/micahnyabuto/swiftshop/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/com/micahnyabuto/swiftshop/ui/navigation/AppNavGraph.kt
@@ -1,36 +1,24 @@
 package com.micahnyabuto.swiftshop.ui.navigation
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.micahnyabuto.swiftshop.ui.Account.AccountScreen
+import com.micahnyabuto.swiftshop.ui.account.AccountScreen
 import com.micahnyabuto.swiftshop.ui.cart.CartScreen
 import com.micahnyabuto.swiftshop.ui.home.HomeScreen
-import com.micahnyabuto.swiftshop.ui.home.ProductViewModel
+import com.micahnyabuto.swiftshop.ui.order.OrderPlacedScreen
+import com.micahnyabuto.swiftshop.ui.productdetails.ProductDetailsScreen
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SwiftShopApp(){
+fun AppNavGraph(){
     val navController = rememberNavController()
 
     Scaffold(
@@ -41,9 +29,13 @@ fun SwiftShopApp(){
             startDestination = Screen.Home.route,
             modifier = Modifier.padding(innerPadding)
         ) {
-            composable(Screen.Home.route) { HomeScreen() }
+            composable(Screen.Home.route) { HomeScreen(
+                navigateToItemDetails = { navController.navigate(ProductDetailsScreen()) }
+            ) }
             composable(Screen.Cart.route) { CartScreen() }
             composable(Screen.Account.route) { AccountScreen() }
+            composable("productDetails") { ProductDetailsScreen() }
+            composable("orderPlaced") { OrderPlacedScreen() }
         }
     }
 }

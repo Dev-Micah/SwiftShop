8fb768613261ecc47577de9fe87979f936ae95ab
Micah Nyabuto <atembamicah@gmail.com>
2025-04-16 23:23:13 +0300

    Update

--- app/src/main/java/com/micahnyabuto/swiftshop/data/SwiftShopRepository.kt
+++ app/src/main/java/com/micahnyabuto/swiftshop/data/SwiftShopRepository.kt
@@ -1,7 +1,10 @@
 package com.micahnyabuto.swiftshop.data
 
+import com.micahnyabuto.swiftshop.data.network.SwiftShopApiService
 
 
 interface SwiftShopRepository {
     suspend fun getProducts(): List<Product>
 }
+
+
